075b71f63b837383e5c39f6dd6d6cc6357d9d2a5
aaraokar <aaraokar>
2017-02-12 18:48:19 +0530

    Removed code comments.

--- src/com/marketpulse/core/relationship/implementation/Person.java
+++ src/com/marketpulse/core/relationship/implementation/Person.java
@@ -5,12 +5,8 @@ import java.util.ArrayList;
 public class Person {
 	private String Name;
 	private String Gender;
-	/*private Person Father;
-	private Person Mother;*/
 	private ArrayList<Person> Parents;
 	private Person Spouse;
-	/*private ArrayList<Person> Sons;
-	private ArrayList<Person> Daughters;*/
 	private ArrayList<Person> Children;
 	
 	public String getName() {
@@ -25,42 +21,12 @@ public class Person {
 	public void setGender(String gender) {
 		Gender = gender;
 	}
-	/*public Person getFather() {
-		return Father;
-	}
-	public void setFather(Person father) {
-		Father = father;
-	}
-	public Person getMother() {
-		return Mother;
-	}
-	public void setMother(Person mother) {
-		Mother = mother;
-	}*/
 	public Person getSpouse() {
 		return Spouse;
 	}
 	public void setSpouse(Person spouse) {
 		Spouse = spouse;
 	}
-	/*public ArrayList<Person> getSons() {
-		return Sons;
-	}
-	public void setSons(ArrayList<Person> sons) {
-		Sons = sons;
-	}
-	public void addSon(Person son) {
-		Sons.add(son);
-	}
-	public ArrayList<Person> getDaughters() {
-		return Daughters;
-	}
-	public void setDaughters(ArrayList<Person> daughters) {
-		Daughters = daughters;
-	}
-	public void addDaughter(Person daughter) {
-		Daughters.add(daughter);
-	}*/
 	public ArrayList<Person> getParents() {
 		return Parents;
 	}
@@ -83,26 +49,15 @@ public class Person {
 		super();
 		Name = "";
 		Gender = "";
-		/*Father = null;
-		Mother = null;*/
 		Spouse = null;
-		/*Sons = new ArrayList<Person>();
-		Daughters = new ArrayList<Person>();*/
 		Parents = new ArrayList<Person>();
 		Children = new ArrayList<Person>();
 	}
-	/*public Person(String name, Person father, Person mother, Person spouse, ArrayList<Person> sons,
-			ArrayList<Person> daughters, ArrayList<Person> children) {*/
-	/*public Person(String name, Person father, Person mother, Person spouse, ArrayList<Person> children) {*/
 	public Person(String name, String gender, Person spouse, ArrayList<Person> parents, ArrayList<Person> children) {
 		super();
 		Name = name;
 		Gender = gender;
-		/*Father = father;
-		Mother = mother;*/
 		Spouse = spouse;
-		/*Sons = sons;
-		Daughters = daughters;*/
 		Parents = parents;
 		Children = children;
 	}
--- src/com/marketpulse/core/relationship/implementation/Relationships.java
+++ src/com/marketpulse/core/relationship/implementation/Relationships.java
@@ -6,127 +6,7 @@ import java.util.Iterator;
 
 public class Relationships {
 	
-	/*private static HashMap<String, String> Father;
-	private static HashMap<String, String> Mother;
-	private static HashMap<String, ArrayList<String>> Sons;
-	private static HashMap<String, ArrayList<String>> Daughters;
-	private static HashMap<String, ArrayList<String>> Spouses;
-	
-	private static volatile Relationships instance;
-	
-	private Relationships() {
-		super();
-		Father = new HashMap<String, String>();
-		Mother = new HashMap<String, String>();
-		Sons = new HashMap<String, ArrayList<String>>();
-		Daughters = new HashMap<String, ArrayList<String>>();
-		Spouses = new HashMap<String, ArrayList<String>>();
-	}
-
-	public HashMap<String, String> getFather() {
-		return Father;
-	}
-	public HashMap<String, String> getMother() {
-		return Mother;
-	}
-	public HashMap<String, ArrayList<String>> getSons() {
-		return Sons;
-	}
-	public HashMap<String, ArrayList<String>> getDaughters() {
-		return Daughters;
-	}
-	public HashMap<String, ArrayList<String>> getSpouses() {
-		return Spouses;
-	}
-	
-	public static Relationships getInstance(){
-		if(instance == null){
-			synchronized (Relationships.class) {
-				if(instance == null){
-					instance = new Relationships();
-				}
-			}
-		}
-		return instance;
-	}
-	
-	public void addFather(String subject, String father){
-		Father.put(subject, father);
-		addSon(father, subject);
-	}
-	public void addMother(String subject, String mother){
-		Mother.put(subject, mother);
-	}
-	public void addSon(String subject, String son){
-		ArrayList<String> listSons = Sons.get(subject);
-		if(listSons == null)
-			listSons = new ArrayList<String>();
-		listSons.add(son);
-		Sons.put(subject, listSons);
-	}
-	public void addDaughter(String subject, String daughter){
-		ArrayList<String> listDaughters = Daughters.get(subject);
-		if(listDaughters == null)
-			listDaughters = new ArrayList<String>();
-		listDaughters.add(daughter);
-		Daughters.put(subject, listDaughters);
-	}
-	public void addSpouse(String subject, String spouse){
-		ArrayList<String> listSpouses = Spouses.get(subject);
-		if(listSpouses == null)
-			listSpouses = new ArrayList<String>();
-		listSpouses.add(spouse);
-		Spouses.put(subject, listSpouses);
-	}
-	public String getParents(String subject){
-		String parents = "";
-		String dad = Father.get(subject);
-		String mom = Mother.get(subject);
-		if(dad != null)
-			parents+=dad;
-		if(mom != null)
-			parents+=", "+mom;
-		return parents;
-	}
-	public String getSpouse(String subject){
-		String spouses = "";
-		ArrayList<String> spouseList = Spouses.get(subject);
-		if(spouseList != null){
-			if(spouseList.size() > 0){
-				spouses += spouseList.get(0);
-				for(int i = 1;i<spouseList.size();i++){
-					spouses += ", "+spouseList.get(i);
-				}
-			}
-		}
-		return spouses;
-	}
-	public String getChildren(String subject){
-		String children = "";
-		ArrayList<String> sonList = Sons.get(subject);
-		if(sonList != null){
-			if(sonList.size() > 0){
-				spouses += spouseList.get(0);
-				for(int i = 1;i<spouseList.size();i++){
-					spouses += ", "+spouseList.get(i);
-				}
-			}
-		}
-		ArrayList<String> spouseList = Spouses.get(subject);
-		if(spouseList != null){
-			if(spouseList.size() > 0){
-				spouses += spouseList.get(0);
-				for(int i = 1;i<spouseList.size();i++){
-					spouses += ", "+spouseList.get(i);
-				}
-			}
-		}
-		return parents;
-	}*/
-	
 	private static HashMap<String, Person> People;
-	
-	/*private static HashMap<String, ArrayList<Person>> Famalies;*/
 	
 	public static HashMap<String, Person> getPeople() {
 		return People;
@@ -135,7 +15,6 @@ public class Relationships {
 	public Relationships() {
 		super();
 		People = new HashMap<String, Person>();
-		/*Famalies = new HashMap<String, ArrayList<Person>>();*/
 	}
 	
 	public void addParent(String subject, String parent, String sex){
@@ -177,34 +56,7 @@ public class Relationships {
 			Parent.addChildren(individual);
 		People.put(subject, individual);
 		People.put(parent, Parent);
-		
-		/*Famalies.put(father, value);*/
 	}
-	/*public void addMother(String subject, String mother){
-		Person mom = People.get(mother);
-		Person individual = People.get(subject);
-		if(mom == null){
-			mom = new Person();
-			mom.setName(mother);
-		}
-		mom.setGender("Female");
-		if(individual == null){
-			individual = new Person();
-			individual.setName(subject);
-		}
-		if(individual.getParents().size() > 0){
-			if(individual.getParents().get(0) != mom){
-				individual.getParents().get(0).setSpouse(mom);
-				mom.setSpouse(individual.getParents().get(0));
-			}
-		}
-		individual.addParent(mom);
-		mom.addChildren(individual);
-		People.put(subject, individual);
-		People.put(mother, mom);
-		
-		Famalies.put(father, value);
-	}*/
 	public void addChild(String subject, String child, String sex){
 		Person individual = People.get(subject);
 		Person Child = People.get(child);
@@ -251,16 +103,6 @@ public class Relationships {
 			if(!exists){
 				Child.addParent(individual.getSpouse());
 			}
-			/*exists = false;
-			for(int i=0;i<individual.getSpouse().getChildren().size();i++){
-				if(individual.getSpouse().getChildren().get(i) == Child){
-					exists = true;
-					break;
-				}
-			}
-			if(!exists){
-				individual.getSpouse().addChildren(Child);
-			}*/
 		}
 		if(Child.getParents().size() > 1){
 			Person spouse1 = Child.getParents().get(0);
@@ -271,46 +113,6 @@ public class Relationships {
 		People.put(subject, individual);
 		People.put(child, Child);
 	}
-	/*public void addDaughter(String subject, String daughter){
-		Person individual = People.get(subject);
-		Person femaleChild = People.get(daughter);
-		if(femaleChild == null){
-			femaleChild = new Person();
-			femaleChild.setName(daughter);
-		}
-		femaleChild.setGender("Female");
-		if(individual == null){
-			individual = new Person();
-			individual.setName(subject);
-		}
-		boolean exists = false;
-		if(individual.getChildren().size() > 0){
-			exists = false;
-			for(int i=0;i<individual.getChildren().size();i++){
-				if(individual.getChildren().get(i) == femaleChild){
-					exists = true;
-					break;
-				}
-			}
-			if(!exists)
-				individual.addChildren(femaleChild);
-		}
-		if(femaleChild.getParents().size() > 0){
-			exists = false;
-			for(int i=0;i<femaleChild.getParents().size();i++){
-				if(femaleChild.getParents().get(i) == individual){
-					exists = true;
-					break;
-				}
-					
-			}
-			if(!exists)
-				femaleChild.addParent(individual);
-		}
-		
-		People.put(subject, individual);
-		People.put(daughter, femaleChild);
-	}*/
 	public void addSpouse(String subject, String spouse){
 		Person individual = People.get(subject);
 		Person partner = People.get(spouse);
